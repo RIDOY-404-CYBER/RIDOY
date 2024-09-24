fedddde03737cfade0645f46cf837f211b0c0179
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2024-09-24 12:09:34 +0600

    Update RIDOY.py

--- RIDOY.py
+++ RIDOY.py
@@ -1,1 +1,8 @@
-
+import os,platform,sys
+os.system("git pull")
+rmx=platform.architecture()[0]
+if rmx=='64bit':
+  import RR
+if rmx=='32bit':
+  import RR2
+else:print('\033[1;32m[•] Contract Admin For Help');exit()
